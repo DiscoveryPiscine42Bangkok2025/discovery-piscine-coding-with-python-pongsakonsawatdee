741c0bdd6c96aa902f1c35d29752cfe198baa020
Pongsakon Sawatdee <67040268@kmitl.ac.th>
2025-09-17 02:11:26 +0700

    Delete cell03/ex03 directory

--- cell03/ex03/i_got_that.py
+++
@@ -1,7 +0,0 @@
-while True:
-    user_input = input("What you gotta say? : ")
-    print("I got that! Anything else? : ", end="")
-
-    # Check if user entered STOP (case-sensitive)
-    if user_input == "STOP":
-        break
